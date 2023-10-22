af82a093a1d8af34ceca81310a8106290f5d88f2
AudunKodehode <audunkodehode@gmail.com>
2023-10-22 20:50:45 +0200

    *

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,26 +1,92 @@
+import { useState, useEffect } from "react";
+import { doc, getDoc, setDoc, onSnapshot } from "firebase/firestore";
+import db from "../firebase";
 import "./Chat.css";
 
-import db from '../firebase'; // Import your Firebase configuration
-import { doc,  setDoc } from 'firebase/firestore';
+export default function Chat() {
+  const [username, setUsername] = useState("");
+  const [messages, setMessages] = useState([]);
+  // Set up a real-time listener to fetch messages
+  useEffect(() => {
+    const docRef = doc(db, "chat", "messages");
+    const unsubscribe = onSnapshot(docRef, (docSnap) => {
+      if (docSnap.exists()) {
+        setMessages(docSnap.data().messages || []);
+      }
+    });
+    // Return a cleanup function to unsubscribe when the component unmounts
+    return () => unsubscribe();
+  }, []);
 
-const sendMessage = async () => {
-    try {
-      const docRef = doc(db, 'chat', 'messages'); // Reference to the single document
-      await setDoc(docRef, { message: {messageInput: document.getElementById('messageInput').value} });
-    } catch (error) {
-      console.error('Error incrementing count:', error);
+  const sendMessage = async () => {
+    //// set username to cookie
+
+    const messageInput = document.getElementById("messageInput");
+
+    const timeNow = new Date();
+    const time = `${timeNow.getHours()}:${timeNow.getMinutes()}`;
+    const messageText = messageInput.value;
+
+    if (messageText !== "" && username !== "") {
+      try {
+        const message = `${time} ${username}: ${messageText}`;
+        const docRef = doc(db, "chat", "messages");
+
+        // Get the current chat messages and append the new message
+        const docSnap = await getDoc(docRef);
+        const currentMessages = docSnap.data().messages || [];
+        currentMessages.push(message);
+
+        // Update the document with the new messages
+        await setDoc(docRef, { messages: currentMessages });
+        messageInput.value = "";
+      } catch (error) {
+        console.error("Error sending message:", error);
+      }
+    } else if (username === "") {
+      //   appendMessage("Error! Please enter a name.");
     }
   };
-  sendMessage(); 
+  addEventListener("DOMContentLoaded", () => {
+    document
+      .getElementById("messageInput")
+      .addEventListener("keydown", (event) => {
+        if (event.key === "Enter") {
+          sendMessage();
+        }
+      });
+    getCookieData();
+  });
 
-export default function Chat() {
+  function getCookieData() {
+    if (document.cookie.length > 0) {
+      document.getElementById("nameInput").value = document.cookie;
+      setUsername(document.cookie);
+    }
+  }
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+    document.cookie = e.target.value;
+  };
+
   return (
     <div className="chat">
       <div className="settings">
-        <input id="nameInput" type="text" placeholder="Name" />
-        <button>Save</button>
+        <label htmlFor="nameInput">Name:</label>
+        <input
+          id="nameInput"
+          type="text"
+          placeholder="Name"
+          onChange={handleChange}
+        />
       </div>
-      <div className="messages"></div>
+      <div id="messages" className="messages">
+        {messages.slice(-20).map((message, index) => (
+          <div className="chatMessage" key={index}>
+            {message}
+          </div>
+        ))}
+      </div>
 
       <div className="newMessage">
         <input id="messageInput" type="text" placeholder="Message" />
--- components/Clicker.jsx
+++ components/Clicker.jsx
@@ -1,32 +1,31 @@
-import  { useState, useEffect } from 'react';
-import { doc, getDoc, setDoc } from 'firebase/firestore';
+import { useState, useEffect } from 'react';
+import { doc, onSnapshot, setDoc } from 'firebase/firestore';
 import db from '../firebase'; // Import your Firebase configuration
-import "./Clicker.css";
+import './Clicker.css';
+
 function Clicker() {
   const [count, setCount] = useState();
 
+  // Reference to the single document
+  const docRef = doc(db, 'counts', 'counter');
+
+  // Set up a real-time listener
   useEffect(() => {
-    const fetchCount = async () => {
-      try {
-        const docRef = doc(db, 'counts', 'counter'); // Reference to the single document
-        const docSnap = await getDoc(docRef);
-        if (docSnap.exists()) {
+    const unsubscribe = onSnapshot(docRef, (docSnap) => {
+      if (docSnap.exists()) {
           setCount(docSnap.data().count);
-        }
-      } catch (error) {
-        console.error('Error fetching count:', error);
       }
-    };
-  
-    fetchCount();
+    });
+
+    // Return a cleanup function to unsubscribe when the component unmounts
+    return () => unsubscribe();
   }, []);
 
   // Function to increment the count in Firestore
   const incrementCount = async () => {
     try {
-      const docRef = doc(db, 'counts', 'counter'); // Reference to the single document
       await setDoc(docRef, { count: count + 1 }); // Update the count in the document
-      setCount(count + 1);
+      
     } catch (error) {
       console.error('Error incrementing count:', error);
     }
